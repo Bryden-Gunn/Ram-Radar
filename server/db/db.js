a70a6b61c7e83863b10c36f5b04eff11bc22b5e3
Taylor Wagstaff <taylorwagstaff@gmail.com>
2022-09-09 10:24:27 +1200

    the Add route and db functions for insert complete

--- server/db/db.js
+++ server/db/db.js
@@ -4,10 +4,17 @@ const connection = require('knex')(config)
 
 module.exports = {
   getRams,
+  addRams,
 }
 
 function getRams(db = connection) {
   return db('rams').select()
 }
 
+// function to add rams to the database.
 
+function addRams(rams, db = connection) {
+  const { plate, latitude, longitude, month, goods } = rams
+  console.log(rams)
+  return db('rams').insert({ plate, latitude, longitude, month, goods })
+}
--- server/routes/rams.js
+++ server/routes/rams.js
@@ -13,4 +13,19 @@ router.get('/', (req, res) => {
     })
 })
 
+router.post('/', (req, res) => {
+  const { plate, latitude, longitude, month, goods } = req.body
+
+  db.addRams({ plate, latitude, longitude, month, goods })
+    .then(() => {
+      // ignore ids from db function
+      res.sendStatus(201)
+      return null
+    })
+    .catch((err) => {
+      console.error(err)
+      res.status(500).json({ message: 'error in server' })
+    })
+})
+
 module.exports = router
